11bf62327ba3f12130bc2326510dc4f55ed08fee
brightjunior475 <brightjunior475@gmail.com>
2021-06-06 13:12:24 -0700

    third commit

---
+++ myphp.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <?php
+ echo("Hello world");
+ echo "<h1>JUNIORS WEBSITE</h1>";
+ echo "<hr>";
+ echo "<p>This is my first website using php</p>"
+ ?>
+ <h4>my name is junior ugochukwu osuji</h4>
+  <?php
+  // Using Variables to change values
+  $CharacterName = "John";
+  $CharacterAge = 35;
+ echo "There is a man called $CharacterName <br>";
+ echo "He was $CharacterAge years old<br>";
+ echo "He likes the name $CharacterName <br>";
+ echo "But didn't like being $CharacterAge years";
+ ?>
+ 
+</body>
+</html>
